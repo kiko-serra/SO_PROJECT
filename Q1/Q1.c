73e94baed0d2a8fd20bc0751659e9cd960f33f4a
kiko-serra <franciscopimentelserra@gmail.com>
2022-11-07 17:41:35 +0000

    Done Q1 and works correctly

--- Q1/Q1.c
+++ Q1/Q1.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
-int j = scanf(user);
-int file = "input from user";
+#include <stdlib.h>
+#include <time.h>
+int main(int argc, char* argv[])
+{
+    if(argc < 3){
+        printf("Usage: samples file numberfrags maxfragsize)\n");
+        return 0;
+    }
+    //Opens file and checks if file exists
+    FILE *fd = fopen(argv[1], "r");
+    if(fd == NULL){
+        printf("Error opening file %s\n", argv[1]);
+        return 0;
+    }
+    //Converts arguments to integers
+    int numberfrags = atoi(argv[2]);
+    int maxfragsize = atoi(argv[3]);
 
-for (int i=0; i < j; i++){
-    printf(fragmentos from file)
+    //Gets file size
+    fseek(fd, 0, SEEK_END);
+    int file_size = ftell(fd);
+    rewind(fd);
+    int maxrandomlimit = file_size - maxfragsize;
+
+    //Functions for random number generation
+    time_t t;
+    srand((unsigned) time(&t));
+
+    for (int i=0; i < numberfrags; i++){
+        int random = rand() % maxrandomlimit;
+        char* buf = malloc(maxfragsize+1);
+        fseek(fd, random, SEEK_SET);
+        fread(buf, maxfragsize, 1, fd);
+        buf[maxfragsize] = '\0';
+        printf(">%s<\n", buf);
+        free(buf);
+    }
+    fclose(fd);
+    return 0;
 }
