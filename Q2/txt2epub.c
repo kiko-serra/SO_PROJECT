9d652f086ddb01087a32819b59459b6a47d3f3bf
PedroCorreia56 <pedromiguelcorreia2002@gmail.com>
2022-11-13 22:48:51 +0000

    Zip a funcionar

--- Q2/txt2epub.c
+++ Q2/txt2epub.c
@@ -9,12 +9,33 @@ int main(int argc, char *argv[]){
         printf("Usage: %s file1 file2 ... filen\n", argv[0]);
         return 0;
     }
+     char zipcommand[10000]="zip ebooks.zip ";
+     for (int i = 1; i < argc; i++)
+     {
+        char c[10000];
+        strcpy(c,argv[i]);
+        char *epub_file=strcat(strtok(c, "."), ".epub ");
+        strcat(zipcommand,epub_file);
+     }
+     
     for (int i = 1; i < argc; i++){
         pid_t pid = fork();
         if (pid == 0){
-            if (execlp("pandoc", "pandoc", argv[i], "-o", strcat(strtok(argv[i], "."), ".epub"), NULL) == -1)
+            printf("[pid%d] converting %s ...\n",getpid(),argv[i]);
+            char string[10000]="pandoc ";
+            strcat(string,argv[i]);
+            strcat(string," -o ");
+            strcat(string,strcat(strtok(argv[i], "."), ".epub"));
+         
+            
+            //strcat("pandoc",strcat(argv[i],strcat(" -o ",strtok(argv[i], "."), ".epub"))
+            if(execlp("/bin/sh","/bin/sh", "-c", string, (char *)NULL)==-1){
+                printf("entrou no if\n");
                 perror("execlp():");
+            }
             printf("Child %d finished\n", i);
+
+            printf("HEELPP\n");
             return 0;
         }
         else if (pid == -1){
@@ -22,9 +43,15 @@ int main(int argc, char *argv[]){
             return 0;
         }
     }
+    //Waiting for the childs to end
     for (int i = 1; i < argc; i++){
         if (wait(NULL) == -1)
             perror("wait():");
     }
+    //executing the zip command
+    if(execlp("/bin/sh","/bin/sh", "-c", zipcommand, (char *)NULL)==-1){
+                perror("execlp():");
+            }
+    
     return 0;
 }
